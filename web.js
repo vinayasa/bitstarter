20bc911930d7e0cba6df28a2aee5fcf6b4606969
Vinaya Sathyanarayana <vinaya.s@gmail.com>
2013-07-10 12:24:59 +0000

    trying to read from index.html

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', 
 	function(request, response) 
 		{
-			response.send( fs.readFileSync ('~/index.html', 'ascii'));
+			response.send( fs.readFileSync ('index.html', 'ascii'));
 
 //					function (err, data)
 //						{
